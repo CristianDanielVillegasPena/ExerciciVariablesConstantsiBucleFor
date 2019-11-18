d14db2c0734c05ec69b2b06f473b83617f6c9dbe
crisvil <utucrisvil@gmail.com>
2019-11-18 13:07:12 +0100

    Fase4

---
+++ FASE4/src/Fase4Todo.java
@@ -0,0 +1,18 @@
+
+public class Fase4Todo {
+
+	public static void main(String[] args) {
+		
+		String nom= "Cristian Daniel " + "Villegas " + "Pena";
+		
+		System.out.println("El meu nom es " + nom);
+		
+		String dia="24/" + "05/" + "2001";
+		
+		System.out.println("Vaig neixer el " + dia);
+		
+		System.out.println("El meu any de naixement no es de trespas");
+
+	}
+
+}
